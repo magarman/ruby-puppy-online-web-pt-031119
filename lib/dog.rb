e66cbff20a9b8c7b836dcab02571853d14d1e091
Mari Garman <garman.mariana@gmail.com>
2019-04-09 02:05:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  @@all = []
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+end
+  
+ def self.all
+    puts @@all.collect{|dog| dog.name}
+  end
+  
+  def self.clear_all
+    @@all.clear
+end
+  
+end
